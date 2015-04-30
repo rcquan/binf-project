c3942d73aec6befe8e11e8b5ad273005151b285a
frankchen07 <frankchen07@gmail.com>
2015-04-30 14:56:48 -0400

    time collapse function

--- sql/load_data.R
+++ sql/load_data.R
@@ -1,11 +1,12 @@
-# devtools::add_path('~/usr/local/bin/psql')
-# devtools::install("~/Downloads/rpostgresql-read-only/RPostgreSQL/")
+devtools::add_path('/Applications/Postgres.app/Contents/Versions/9.4/bin/')
+devtools::install("~/Downloads/rpostgresql-read-only/RPostgreSQL/")
+
 library(dplyr)
 db <- src_postgres(dbname = "MIMIC2",
-                  host = "ec2-52-11-227-94.us-west-2.compute.amazonaws.com",
+                  host = "ec2-54-163-173-71.compute-1.amazonaws.com",
                   port = 5432,
                   user = "ec2-user",
-                  password = "mimic1234")
+                  password = "thisisalongpassword1234")
 
 qControlID <- sql(
 "SELECT subject_id
@@ -23,5 +24,22 @@ FROM mimic2v26.icd9
 WHERE code LIKE '995.9%' OR code = '785.52'"
 )
 
-controlID <- tbl(db, qControlID)
+qCE <- sql(
+"SELECT * 
+FROM mimic2v26.chartevents 
+WHERE itemid in (615, 646, 6, 51, 455, 6701, 211, 50316, 50468, 20002, 920, 3580) 
+ORDER BY subject_id
+LIMIT 500"
+)
+
+controlID <- tbl(db, qControlID) %>% arrange(subject_id)
 collect(controlID)
+
+df <- tbl(db, qCE) %>% group_by(subject_id, icustay_id) %>% select(charttime, icustay_id, itemid, value1num, value2) %>% collect()
+ground <- df %>% filter(row_number()==1)
+test <- df %>% mutate(intHour=as.numeric(difftime(charttime, ground$charttime[subject_id], tz="EST", units="hours")),
+					  intHourR=round(intHour))
+test %<>% group_by(subject_id, icustay_id, itemid, intHourR) %>% summarize(value1Mean=mean(value1num),
+																		  value1Sd=sd(value1num))
+
+
